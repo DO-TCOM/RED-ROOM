2dcad43444ec7457a088e9044f1c6261e9c9becc
DOTCOM <marcoponio7@gmail.com>
2026-03-04 12:55:30 -0500

    Change RedRoom route to serve index.html

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
+
+// Route RedRoom ✅
 app.get('/RedRoom', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'RedRoom.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 let users = {};
