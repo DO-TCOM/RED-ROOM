20861fad6a5e382d48dec866aef25b232ed245e8
DOTCOM <marcoponio7@gmail.com>
2026-03-04 12:45:08 -0500

    Add route to serve RedRoom.html

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
+app.get('/RedRoom', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'RedRoom.html'));
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 let users = {};
